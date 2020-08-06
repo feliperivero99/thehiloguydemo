f1d44677c7fe3bcadd9579017b850af7fb127cae
feliperivero99 <felipe.riverot@gmail.com>
2020-08-06 10:23:58 -0500

    Version 1

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $(".zoom").hover(function(){
+		
+		$(this).addClass('transition');
+	}, function(){
+        
+		$(this).removeClass('transition');
+	});
+});
